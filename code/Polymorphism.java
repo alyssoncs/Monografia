f3c66be63be7c1c9b186b8c48548a07964b4e338
alysson <alysson.cirilo@gmail.com>
2019-07-12 13:08:04 -0300

    alterações na solução

--- code/Polymorphism.java
+++ code/Polymorphism.java
@@ -1,13 +1,14 @@
 Subscriber = SubscriberFactory.createSubscriber();
 subscriber.addConnection(connection); //connection with mqtt broker
-subscriber.subscribeServiceByName("Temperature");
+subscriber.subscribeObjectConnectedTopic();
+subscriber.subscribeObjectDisconnectedTopic();
 
 subscriber.setSubscriberListener(new ISubscriberListener() {
 	@Override
 	public void onMessageArrived(Message message) {
-		if (message instanceof ObjectFoundMessage) {
+		if (message instanceof ObjectConnectedMessage) {
 			/* do something */
-		} else if (message instanceof ObjectConnectedMessage) {
+		} else if (message instanceof ObjectDisconnectedMessage) {
 			/* do somethinge else */
 		}
 	}
